221a6695464dcd3005ad5839890a8133118ade55
임빈영 <qlsdud960604@gmail.com>
2022-10-19 10:45:05 +0900

    Add #3 - RequestLine
    요청 라인을 처리하는 로직을 별도의 클래스로 분리

---
+++ src/main/java/http/RequestLine.java
@@ -0,0 +1,51 @@
+package http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestLine {
+    private static final Logger log = LoggerFactory.getLogger(RequestLine.class);
+
+    private String method;   // 요청 메서드(GET, POST)
+    private String path;   // 요청 URL
+    private Map<String, String> params = new HashMap<String, String>();   // 요청 파라메터
+
+    public RequestLine(String requestLine) {
+        log.debug("===== request line : {}=====", requestLine);   // 요청 라인
+
+        String[] tokens = requestLine.split(" ");
+        if (tokens.length != 3) {
+            throw new IllegalArgumentException(requestLine + "이 형식에 맞지 않습니다.");
+        }
+
+        method = tokens[0];
+        if (method.equals("POST")) {
+            path = tokens[1];
+            return;
+        }
+
+        int index = tokens[1].indexOf("?");
+        if (index == -1) {
+            path = tokens[1];
+        } else {
+            path = tokens[1].substring(0, index);
+            params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
